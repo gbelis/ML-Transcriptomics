7c0b19600391f0f2c5ad2b7ed52c546741fe0898
Guillaume Belissent <guillaumebelissent@tsf-436-wpa-1-186.epfl.ch>
2022-11-27 18:22:28 +0100

    shapes and colors

--- main.jl
+++ main.jl
@@ -9,10 +9,10 @@ test_df = DataFrame(CSV.File("./data/test.csv.gz"))
 x = clean_data(vcat(select(train_df, Not(:labels)), test_df))
 y = coerce!(train_df, :labels => Multiclass).labels
 x_train = x[1:5000,:]
-x_pred = x[5001:end,:]
+x_test = x[5001:end,:]
 
 mach = machine(MultinomialClassifier(penalty = :none), x_train, y) |> fit!
 pred_train = predict_mode(mach, x_train)
 mean(pred_train  .== y)
-pred = predict_mode(mach, x_pred)
+pred = predict_mode(mach, x_test)
 kaggle_submit(pred, "MultinomialClassifier_27_11")
--- models.jl
+++ models.jl
@@ -7,4 +7,6 @@ function multinom_class(x_train, x_test, y)
     mach = machine(MultinomialClassifier(penalty = :none), x_train, y) |> fit!
     pred = predict_mode(mach, x_test)
     kaggle_submit(pred, "MultinomialClassifier_27_11")
+    return
+end
 
