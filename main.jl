5f5268517577452363ed10099d495b1fa1fb6707
Guillaume Belissent <guillaumebelissent@tsf-436-wpa-1-186.epfl.ch>
2022-11-27 19:13:01 +0100

    shapes and colors

--- main.jl
+++ main.jl
@@ -1,18 +1,33 @@
 using Pkg; Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
-using Plots, DataFrames, Random, CSV, MLJ, MLJLinearModels, MLCourse, Statistics, Distributions,OpenML
+using Plots, DataFrames, Random, CSV, MLJ, MLJLinearModels, MLCourse, Statistics, Distributions,OpenML, NearestNeighborModels
 include("./utility.jl")
 include("./models.jl")
 
-train_df = DataFrame(CSV.File("./data/test.csv.gz"))
+train_df = DataFrame(CSV.File("./data/train.csv.gz"))
 test_df = DataFrame(CSV.File("./data/test.csv.gz"))
 
-x = clean_data(vcat(select(train_df, Not(:labels)), test_df))
-y = coerce!(train_df, :labels => Multiclass).labels
-x_train = x[1:5000,:]
-x_test = x[5001:end,:]
 
+x_train = clean_data(select(train_df, Not(:labels)))
+x_test = clean_data(select(test_df, names(x_train)))
+x_train = select(train_df, names(x_test))
+
+y = coerce!(train_df, :labels => Multiclass).labels
+
+"""
 mach = machine(MultinomialClassifier(penalty = :none), x_train, y) |> fit!
 pred_train = predict_mode(mach, x_train)
 mean(pred_train  .== y)
 pred = predict_mode(mach, x_test)
-kaggle_submit(pred, "MultinomialClassifier_27_11")
+kaggle_submit(pred, "MultinomialClassifier_27_11_v2")
+"""
+
+
+"""
+model = MultitargetKNNClassifier(output_type = ColumnTable)
+self_tuning_model = TunedModel(model = model, resampling = CV(nfolds = 5), tuning = Grid(),
+                    range = range(model, :K, values = 1:50), measure = MisclassificationRate)
+
+KNN_tuned = machine(self_tuning_model, x_train, y, scitype_check_level=0)
+fit!(KNN_tuned, verbosity = 0)
+
+"""
