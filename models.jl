a701de98b791318722651892a06030ba96598a29
CamilleChallier <camille.challier@epfl.ch>
2022-12-07 10:52:26 +0100

    gz

--- models.jl
+++ models.jl
@@ -1,7 +1,3 @@
-using Pkg
-    Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
-using Plots, DataFrames, Random, CSV, MLJ, MLJLinearModels, MLCourse, Statistics
-
 function fit_and_evaluate(training_data, validation_data, test_data,validation_test)
     """
         fit the data with a multinomailClassifier and evaluate it.
@@ -17,9 +13,16 @@ function fit_and_evaluate(training_data, validation_data, test_data,validation_t
         error {DataFrame} -- dataframe of training and test error
 
     """
-    mach = machine(MultinomialClassifier(penalty = :l2, lambda = 1e-4), training_data, validation_data) |> fit!
-    return mach, DataFrame(training_error = sum(MLJ.predict(mach).== validation_data),
-    test_error = sum(MLJ.predict(mach, test_data).== validation_test), errorate = mean(predict_mode(mach, test_data) .!= validation_test))
+    mach = machine(TunedModel(model=RandomForestClassifier(),
+                                            resampling = CV(nfolds=3),
+                                            ranges = [range(RandomForestClassifier(), :n_trees, values=[10,100,500,1000,1500]),
+                                                     range(RandomForestClassifier(), max_depth, values=[-1,5,10])],
+                                            measure = MisclassificationRate()),
+                                            training_data, validation_data) |>fit!
+
+    #mach = machine(RandomForestClassifier(n_trees=100,max_depth = 50),training_data, validation_data) |>fit!
+    error = DataFrame(trainin_error = mean(predict_mode(mach, training_data) .!= validation_data), test_error = mean(predict_mode(mach, test_data) .!= validation_test))
+    return mach, error
 end
 
 function data_split(data,y, idx_train, idx_test; shuffle =true)
@@ -67,7 +70,8 @@ function multinom_class(x_train, x_test, y; pena, lambda, title)
     """
     #mach = machine(MultinomialClassifier(penalty = pena, lambda = lambda), x_train, y) |> fit!
     mach = machine(LogisticClassifier(penalty = pena, lambda = lambda), x_train, y) |> fit!
-    pred = predict_mode(mach, x_tests)
+    pred = predict_mode(mach, x_test)
+    printl(pred)
     kaggle_submit(pred, "MultinomialClassifier_pca2_$(pena)_$(title)_30_11")
     return mach
 end
@@ -99,7 +103,7 @@ function lasso_classifier(x_train, x_test, y;seed=0, goal, lower, upper)
                                     measure = MisclassificationRate()),
                                     x_train, y) |>fit!
     pred = predict_mode(mach_lasso, x_test)
-    kaggle_submit(pred, "LassoClassifier_30_11")  
+    kaggle_submit(pred, "LassoClassifier_pca8000_7_5")
     return mach_lasso
 end
 
@@ -129,35 +133,64 @@ function ridge_classifier(x_train, x_test, y; seed=0, goal, lower, upper)
                                     measure = MisclassificationRate())
     mach_ridge = machine(tuned_model_ridge, x_train, y) |>fit!
     pred = predict_mode(mach_ridge, x_test)
-    kaggle_submit(pred, "RidgeClassifier_30_11")  
+
+    kaggle_submit(pred, "RidgeClassifier_30_11")
     return mach_ridge
 end
-   
-function XGboost_class(x_train, x_test, y)
 
-    pipe = @pipeline(Standardizer, OneHotEncoder)#, cfr())
-    pipe.xg_boost_classifier.num_round = 200
-    pipe.xg_boost_classifier.subsample = 0.8
-    pipe.xg_boost_classifier.colsample_bytree = 0.8
-    mach = machine(TunedModel(model = pipe,
-                            resampling = CV(nfolds = 4),
-		                    measure = MisclassificationRate(),
-                            tuning = Grid(goal = 25),
-                            range = [range(pipe, :eta, lower = 1e-2, upper = .1, scale = :log),
-                                     range(pipe, :num_round, lower = 50, upper = 500),
-                                     range(pipe, :max_depth, lower = 2, upper = 6)]),
-                  x_train, y)
-	fit!(mach, verbosity = 0)
-   
-    kaggle_submit(pred, "MultinomialClassifier_$(pena)_27_11")
+function RandomForest_classifier(x_train, x_test, y; seed=0)
+    """
+        RandomForest Classification using cross-validation. Save the prediction in a csv file.
+
+    Arguments:
+        x_train {DataFrame} -- train set (without labels)
+        x_test {DataFrame} -- test set to predict
+        y {DataFrame} -- labels of the training data
+        seed {int} -- value of the seed to fix
+
+    Returns:
+        mach{} -- machine
+
+    """
+    Random.seed!(seed)
+    mach_forest = machine(TunedModel(model=RandomForestClassifier(),
+                                            resampling = CV(nfolds=3),
+                                            ranges = [range(RandomForestClassifier(), :n_trees, values=[10,100,500,1000,1500]),
+                                                     range(RandomForestClassifier(), :max_depth, values=[-1,5,10])],
+                                            measure = MisclassificationRate()),
+                                            x_train, y) |>fit!
+
+    training_error = mean(predict_mode(mach_forest, x_train) .!= y)
+    pred = predict_mode(mach_forest, x_test)
+    kaggle_submit(pred, "RandomForestClassifier_3_12")
+    return mach_forest, training_error
 end
+
+# function XGboost_class(x_train, x_test, y)
+
+#     pipe = @pipeline(Standardizer, OneHotEncoder)#, cfr())
+#     pipe.xg_boost_classifier.num_round = 200
+#     pipe.xg_boost_classifier.subsample = 0.8
+#     pipe.xg_boost_classifier.colsample_bytree = 0.8
+#     mach = machine(TunedModel(model = pipe,
+#                             resampling = CV(nfolds = 4),
+# 		                    measure = MisclassificationRate(),
+#                             tuning = Grid(goal = 25),
+#                             range = [range(pipe, :eta, lower = 1e-2, upper = .1, scale = :log),
+#                                      range(pipe, :num_round, lower = 50, upper = 500),
+#                                      range(pipe, :max_depth, lower = 2, upper = 6)]),
+#                   x_train, y)
+# 	fit!(mach, verbosity = 0)
+
+#     kaggle_submit(pred, "MultinomialClassifier_$(pena)_27_11")
+# end
 
 
     #r = [
     #    range(pipe, :(xg_boost_classifier.eta), lower=0.001, upper=0.03, scale=:log),
     #    range(pipe, :(xg_boost_classifier.max_depth), lower=2, upper=10)
     #    ]
-    
+
     #pipe.xg_boost_classifier.num_round = 200
     #pipe.xg_boost_classifier.subsample = 0.8
     #pipe.xg_boost_classifier.colsample_bytree = 0.8
@@ -176,5 +209,5 @@ function multi_knn(x_train,y) #Trash
                         range = range(model, :K, values = 1:50), measure = MisclassificationRate)
     KNN_tuned = machine(self_tuning_model, x_train, y, scitype_check_level=0)
     fit!(KNN_tuned, verbosity = 0)
-    
+
 end
